14d235c3ffacfa3655af289989fe4a5b4d259511
ramuering <sugarone0426@gmal.com>
2023-11-17 20:58:45 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,84 @@
+import React, { useState } from "react";
+import "./App.css";
+import Board from "./components/Board";
+
+function App() {
+  const [history, setHistory] = useState([{ squares: Array(9).fill(null) }]);
+  const [xIsNext, setIsNext] = useState(true);
+  const [stepNumber, setStepNumber] = useState(0);
+
+  const calculateWinner = (squares) => {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (const element of lines) {
+      const [a, b, c] = element;
+      if (
+        squares[a] &&
+        squares[a] === squares[b] &&
+        squares[a] === squares[c]
+      ) {
+        return squares[a];
+      }
+    }
+    return null;
+  };
+  const current = history[stepNumber];
+  const winner = calculateWinner(current.squares);
+
+  let status;
+  if (winner) {
+    status = "Winner : " + winner;
+  } else {
+    status = "Next player : " + (xIsNext ? "X" : "O");
+  }
+  const handleClick = (i) => {
+    const newHistory = history.slice(0, stepNumber + 1);
+    const newCurrent = newHistory[newHistory.length - 1];
+    const newSquares = newCurrent.squares.slice();
+    if (calculateWinner(newSquares) || newSquares[i]) {
+      //승자가 발생 or 이미 값이 있음
+      return;
+    }
+    newSquares[i] = xIsNext ? "X" : "O";
+    setHistory([...history, { squares: newSquares }]);
+    setIsNext((prev) => !prev); //current는 원하는 이름으로 변경 가능
+
+    setStepNumber(newHistory.length);
+  };
+
+  const moves = history.map((step, move) => {
+    const desc = move ? "Go to move #" + move : "Go to game start";
+    return (
+      <li key={move}>
+        <button className="move-button" onClick={() => jumpTo(move)}>
+          {desc}
+        </button>
+      </li>
+    );
+  });
+  const jumpTo = (step) => {
+    setStepNumber(step);
+    setIsNext(step % 2 === 0);
+  };
+  return (
+    <div className="game">
+      <div className="game-board">
+        <Board squares={current.squares} onClick={(i) => handleClick(i)} />
+      </div>
+      <div className="game-info">
+        <div className="status">{status}</div>
+        <ol style={{ listStyle: "none" }}>{moves}</ol>
+      </div>
+    </div>
+  );
+}
+
+export default App;
